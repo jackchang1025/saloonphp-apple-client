767b7e36c347dfd785149ba100d01ee1001e7c50
weijiajia <123456@qq.com>
2025-03-17 17:55:39 +0800

    feat(ValidateAccountFieldsSrvData): 扩展账户验证数据类，新增个人信息字段
    - 在 ValidateAccountFieldsSrvData 类中新增 firstName、lastName、birthMonth、birthDay、birthYear 和 agreedToTerms 字段，以支持更全面的账户验证信息。
    - 添加示例数据注释，便于理解和使用该类。

--- src/Integrations/BuyTvApple/Data/ValidateAccountFieldsSrvData.php
+++ src/Integrations/BuyTvApple/Data/ValidateAccountFieldsSrvData.php
@@ -6,9 +6,31 @@ use Weijiajia\SaloonphpAppleClient\DataConstruct\Data;
 
 class ValidateAccountFieldsSrvData extends Data
 {
+    //storefront: USA
+// context: create
+// firstName: Jack
+// lastName: Chang
+// birthMonth: 10
+// birthDay: 25
+// birthYear: 1996
+// acAccountName: DeborahWhitep8uzz@gmail.com
+// acAccountPassword: *7@e@rljDlC,)p*-u$vi
+// marketing: 1
+// restrictedAccountType: restrictedEmailOptimizedWeb
+// addressOfficialCountryCode: USA
+// paymentMethodType: None
+// pageUUID: o2a+kdfx25UoZb2hf+I42t8E5S8=
+// agreedToTerms: 1
+// accountType: email
+// email: DeborahWhitep8uzz@gmail.com
     public function __construct(
         public string $email,
         public string $acAccountName,
+        public string $firstName,
+        public string $lastName,
+        public int $birthMonth,
+        public int $birthDay,
+        public int $birthYear,
         public string $acAccountPassword,
         public string $pageUUID,
         public string $storefront = 'USA',
@@ -18,6 +40,7 @@ class ValidateAccountFieldsSrvData extends Data
         public string $addressOfficialCountryCode = 'USA',
         public string $paymentMethodType = 'None',
         public string $accountType = 'email',
+        public int $agreedToTerms = 1,
         
     ) {}
 }
